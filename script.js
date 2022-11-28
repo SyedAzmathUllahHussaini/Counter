018a3364705c45dd091482f75b8c6afd07e0e316
SyedAzmathUllahHussaini <azmathullah1604@gmail.com>
2022-11-27 22:56:56 -0600

    initial message

---
+++ script.js
@@ -0,0 +1,17 @@
+var counter = document.querySelector(".counter");
+var followers = document.querySelector(".followers");
+
+
+let count = 1;
+
+setInterval(() => {
+    if(count <= 1000) {
+        count++;
+        counter.innerText = count;
+    }
+}, 10);
+
+setTimeout (() => {
+    followers.innerText = "Followers on linkedin";
+
+},10000);
